bee183d213deb6c2b3a217a1fd44188e8a436c19
mesvak <aria.shobeiri1@live.vu.edu.au>
2026-08-25 16:43:57 +1000

    again

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -10,6 +10,7 @@ const pool = dbUrl
   ? mysql.createPool({
       uri: dbUrl,
       waitForConnections: true,
+      multipleStatements: true, 
       connectionLimit: 10,
       queueLimit: 0,
       dateStrings: true,
